983b7140dcee63bdcc1fcdb04bf3d4d4b715caf6
ccofre <ccofre@subcargo.com>
2023-06-27 13:04:38 -0400

    Ordena código y corrige estilos

--- Tareas/Tarea08/assets/js/scripts.js
+++ Tareas/Tarea08/assets/js/scripts.js
@@ -14,15 +14,15 @@
 
 // se define la clase personaje
 class Personaje {
-  constructor(nombre, especie, imagen, estado) {
+  constructor(nombre, especie, imagen, estado, genero) {
     this._nombre = nombre;
     this._especie = especie;
     this._imagen = imagen;
     this._estado = estado;
+    this._genero = genero;
   }
 
   get nombre() {
-    console.log("estoy usando el nombre")
     return this._nombre;
   }
 
@@ -54,6 +54,43 @@ class Personaje {
     this._estado = nuevoEstado;
   }
 
+  get genero() {
+    return this._genero;
+  }
+
+  set genero(nuevoGenero) {
+    this._genero = nuevoGenero;
+  }
+
+  // Método para devolver la especie en español si es conocida. Si no lo es, se devuelve la original en inglés
+  getEspecieEs(){
+    const dictEspecies = {
+      "Human": "Humano",
+      "Mythological Creature": "Criatura mitológica",
+      "Humanoid": "Humanoide",
+      "Disease": "Enfermedad",
+      "unknown": "Desconocido"
+    }
+    if(dictEspecies[this._especie])
+      return dictEspecies[this._especie]
+    else
+      return this._especie
+  }
+
+  // Método para devolver el género en español si es conocido. Si no lo es, devuelve el original en inglés.
+  getGeneroEs(){
+    const dictGeneros = {
+      "Male": "Masculino",
+      "Female": "Femenino",
+      "unknown": "Género Desconocido"
+    }
+    if(dictGeneros[this._genero])
+      return dictGeneros[this._genero]
+    else
+      return this._genero
+  }
+
+  // 'consologuea' el objeto
   print() {
     console.log(this)
   }
@@ -61,16 +98,20 @@ class Personaje {
   // show toma el contenido de un elemento del dom de id=contenedor y agrega la card del 
   // personaje
   show(contenedor) {
+      
     document.getElementById(contenedor).innerHTML +=
-      `<div class="card">
-        <img src="${this._imagen}" alt="${this._nombre}">
-        <div class="card-content">
-          <h2>${this._nombre}</h2>
-          <ul class="character-info">
-            <li class=${this._estado}>${this._especie}</li>
-          </ul>
-        </div>
-      </div>`;
+    `<div class="card">
+      <img src="${this._imagen}" alt="${this._nombre}">
+      <div class="card-content">
+        <h2>${this._nombre}</h2>
+        <ul class="character-info">
+          <li class="${this._estado==="Alive"?"Verde":(this._estado=='Dead'?"Rojo":"unknown")}"><i class="fa-solid ${this._estado==='Dead'?"fa-skull-crossbones":(this._estado=='Alive'?"fa-heart-pulse":"fa-circle-question")} fa-xs"></i> ${this.getEspecieEs()}</li>
+          <li class="${this._genero=="Male"?"Azul":(this._genero=="Female"?"Rojo":"unknown")}"><i class="fa-solid ${this._genero=="Male"?"fa-mars":(this._genero=="Female"?"fa-venus":"fa-circle-question")} fa-xs"></i> ${this.getGeneroEs()}</li>
+        </ul>
+        <ul>
+        </ul>
+      </div>
+    </div>`;
   }
 }
 
@@ -78,17 +119,16 @@ class Personaje {
 // función que obtiene información de un personaje y lo carga en una instancia de
 // la clase "Personaje", el parámetro es el id asignado a cada personaje por el sitio
 // corresponde a un entero positivo.
-
 async function getPersonaje(id) {
-  const URL = `https://rickandmortyapi.com/api/character/${id}`
 
+  const URL = `https://rickandmortyapi.com/api/character/${id}`
   try {
     // Consulta a la API
     const rawPersonaje = await fetch(URL);
     const PersonajeJson = await rawPersonaje.json();
     
     // se crea objeto personaje y se devuelve como resultado de la función
-    const personaje = new Personaje(PersonajeJson.name, PersonajeJson.species, PersonajeJson.image, PersonajeJson.status);
+    const personaje = new Personaje(PersonajeJson.name, PersonajeJson.species, PersonajeJson.image, PersonajeJson.status, PersonajeJson.gender);
     return personaje;
     
   } catch (error) {
@@ -97,9 +137,8 @@ async function getPersonaje(id) {
 }
 
 
-
 // se inyectan 20 personajes
-for(let i=1; i<=20; i++)
+for(let i=1; i<=200; i++)
   getPersonaje(i).then(personaje => {
     personaje.show('contenedor')
     personaje.print()
